771ca6091fcc1bd81bce199cfba65018ed060ab5
= <=>
2016-11-21 13:30:52 +0100

    STEP 67 - Creating the Shopping List Service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared';
+
+@Injectable()
+export class ShoppingListService {
+  private items: Ingredient[] = [];
+
+  constructor() { }
+
+  geItems() {
+    return this.items;
+  }
+
+  addItems(items: Ingredient[]) {
+    //Array.prototype.push.apply(this.items, items);
+    this.items.push(... items);
+  }
+
+}
