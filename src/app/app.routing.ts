5a7e58bec55500b1a513247b1a3a0354ca946262
NicolasBouhours <nico53960@hotmail.fr>
2016-11-22 23:23:23 +0100

    STEP 100 - fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RECIPES_ROUTES } from './recipes/recipes.routes';
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent, children: RECIPES_ROUTES },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
