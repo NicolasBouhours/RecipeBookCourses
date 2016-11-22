eb8dc0019fa0281794b907a51d1bc6299f2c8558
NicolasBouhours <nico53960@hotmail.fr>
2016-11-22 22:17:21 +0100

    STEP 93 - Setting up the Main Routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+const APP_ROUTES: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shoppibg-list', component: ShoppingListComponent }
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
