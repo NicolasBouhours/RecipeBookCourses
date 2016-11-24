62d2d9c436060fdce232e8c29d63b3dacc1bc994
NicolasBouhours <nico53960@hotmail.fr>
2016-11-24 22:03:58 +0100

    STEP 142 - Submitting the Form and Editing Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { Recipe } from '../recipe';
 import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
@@ -17,7 +17,10 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   private isNew = true;
   private subscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private formBuilder: FormBuilder) { }
+  constructor(private route: ActivatedRoute, 
+              private recipeService: RecipeService, 
+              private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
     this.subscription = this.route.params.subscribe(
@@ -64,6 +67,24 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
       description: [recipeContent, Validators.required],
       ingredients: recipeIngredients
     });
+  }
+
+  onSubmit() {
+    const newRecipe = this.recipeForm.value;
+    if (this.isNew) {
+      this.recipeService.addRecipe(newRecipe);
+    } else {
+      this.recipeService.editRecipe(this.recipe, newRecipe);
+    }
+    this.navigateBack();
+  }
+
+  onCancel() {
+    this.navigateBack();
+  }
+
+  private navigateBack() {
+    this.router.navigate(['../']);
   }
 
   ngOnDestroy() {
