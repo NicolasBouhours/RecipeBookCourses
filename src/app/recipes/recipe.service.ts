d67b0e658f899481273964259c568a66e1b8b174
NicolasBouhours <nico53960@hotmail.fr>
2016-11-27 18:57:50 +0100

    STEP 168 - Storing Recipes on the Server (POST)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Headers } from '@angular/http';
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
 
@@ -13,7 +13,7 @@ export class RecipeService {
     new Recipe('Walioa', 'Nicolas', 'http://static.lexpress.fr/medias_9078/w_2048,h_890,c_crop,x_0,y_423/w_1520,h_855,c_fill,g_north/v1391590050/burger-3_4648336.jpg', [])
   ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -33,6 +33,18 @@ export class RecipeService {
 
   deleteRecipe(recipe: Recipe) {
     this.recipes = this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.post('https://recipe-book-8fa8a.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+
   }
 
 }
